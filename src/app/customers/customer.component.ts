8ab0631e1c94e3dd9b542ee472680839758d438f
Dave Harms <dharms@cic.com>
2017-09-20 11:13:57 -0400

    got to 5.6

---
+++ src/app/customers/customer.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { 
+    FormGroup, 
+    FormBuilder, 
+    Validators, 
+    AbstractControl, 
+    ValidatorFn 
+} from '@angular/forms';
+
+import { Customer } from './customer';
+
+function ratingRange(min: number, max: number): ValidatorFn {
+    return (c: AbstractControl): {[key: string]: boolean} | null => {
+        if (c.value != undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
+            return { 'range': true}
+        };
+        return null;
+    };
+}
+
+@Component({
+    selector: 'my-signup',
+    templateUrl: './customer.component.html'
+})
+export class CustomerComponent implements OnInit  {
+    customerForm: FormGroup;
+    customer: Customer = new Customer();
+
+    constructor(private fb: FormBuilder) { }
+
+    ngOnInit(): void {
+        this.customerForm = this.fb.group({
+            firstName: ['', [Validators.required, Validators.minLength(3)]],
+            lastName: ['', [Validators.required, Validators.maxLength(50)]],
+            email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+')]],
+            phone: '',
+            notification: 'email',
+            rating: ['', ratingRange(1,5)],
+            sendCatalog: true
+        });
+    }
+
+    populateTestData(): void {
+        this.customerForm.patchValue({
+            firstName: 'Jack',
+            lastName: 'Harkness',
+            // email: 'jack@torchwood.com',
+            sendCatalog: false
+        })
+    }
+
+    save() {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
+
+    setNotification(notifyVia: string): void {
+        const phoneControl = this.customerForm.get('phone');
+        if (notifyVia === 'text') {
+            phoneControl.setValidators(Validators.required);
+        } else {
+            phoneControl.clearValidators();
+        }
+        phoneControl.updateValueAndValidity();
+    }
+ }
